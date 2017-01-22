47647fe29e3dd1129ee26213ea17664f8d7a8ebd
Victor Ferraz <victorfsf.dev@gmail.com>
2017-01-22 13:07:16 -0300

    [master] cdpath.mo -> cdpath.mo.sh

--- render.sh
+++ render.sh
@@ -6,7 +6,7 @@ expstr="export CDPATH=\".\`cat \"\$HOME/.cdpath\" | sed ':a;N;\$!ba;s/\\\n//g'\`
 grepstr="export CDPATH=\".\`cat \"\$HOME/.cdpath\" | sed ':a;N;\$!ba;s/\\\n//g'\`\"" \
 paths='`echo ${paths//\"/}`' \
 choice='$choice:l' \
-./lib/mo cdpath.mo > ../cdpath.zsh.sh
+./lib/mo cdpath.mo.sh > ../cdpath.zsh.sh
 
 
 echo 'Rendering the BASH Template...'
@@ -15,6 +15,6 @@ expstr='export CDPATH=".`cat "$HOME/.cdpath" | sed ":a;N;$!ba;s/\n//g"`"' \
 grepstr='export CDPATH=".`cat "$HOME/.cdpath" | sed ":a;N;\$!ba;s\/\\n//g"`"' \
 paths='$paths' \
 choice='${choice,,}' \
-./lib/mo cdpath.mo > ../cdpath.bash.sh
+./lib/mo cdpath.mo.sh > ../cdpath.bash.sh
 
 echo -e "\nDone."
