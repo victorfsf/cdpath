0491fead551e0aecc3ae147b0fdbdf000380d2c1
Victor Ferraz <victorfsf.dev@gmail.com>
2017-11-07 10:05:45 -0300

    [new] Updating install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
 install() {
+    if [ -z "$1" ]; then
+        printf "install.sh takes exactly 1 argument (0 given)\n"
+        printf "usage: ./install.sh <shell>\n"
+        printf "       (e.g. \"./install.sh ~/.bashrc\")\n"
+        return 1
+    fi
     declare shell="$1"
-    curl https://raw.githubusercontent.com/victorfsf/cdpath/master/cdpath.sh > /usr/local/bin/cdpath.sh
-    echo 'source "/usr/local/bin/cdpath.sh"' >> "$shell"
+    curl -s https://raw.githubusercontent.com/victorfsf/cdpath/master/cdpath.sh > /usr/local/bin/cdpath.sh
+    cat "$shell" | grep 'source "/usr/local/bin/cdpath.sh"' >/dev/null
+    if [ ! $? -eq 0 ]; then
+        echo 'source "/usr/local/bin/cdpath.sh"' >> "$shell"
+    fi
     source /usr/local/bin/cdpath.sh
     cdpath -i "$shell"
 }
